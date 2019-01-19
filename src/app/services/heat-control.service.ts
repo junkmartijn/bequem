42d3476aaba7264fc2cf35193c5f4bc5dc33c453
Martijn <Martijn@LAPTOPM>
2019-01-19 13:28:07 +0100

    cleanup

--- src/app/services/heat-control.service.ts
+++ src/app/services/heat-control.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@angular/core';
 import { MessageService } from './message.service';
 import { Task } from '../models/task';
-import { HeatStatus } from '../models/heat-status';
 import { HeatStatusResponse } from '../models/heat-status-response';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable, of } from 'rxjs';
@@ -19,83 +18,17 @@ export class HeatControlService {
 
   private heatControlBaseUrl = 'http://demo5335077.mockable.io/';
   private heatControlTasksUrl = this.heatControlBaseUrl + 'api/tasks';
-  private heatControlOverridesUrl = this.heatControlBaseUrl + 'api/overrides';
-
-  //private heatControlBaseUrl = 'http://192.168.1.150/';
-  //private heatControlTasksUrl = this.heatControlBaseUrl + 'api/tasks';
-  //private heatControlOverridesUrl = this.heatControlBaseUrl + 'api/overrides';
-  private heatStatusUrl = 'http://192.168.1.150/heat_status';
+  private heatControlOverrideUrl = 'http://192.168.1.150/overrides?onzin=1&';
+  private heatControlStatusUrl = 'http://192.168.1.150/heat_status';
 
   constructor(
     private http: HttpClient,
     private messageService: MessageService) { }
 
   getHeatStatuses(): Observable<HeatStatusResponse> {
-    //this.http.request("GET", 'http://192.168.1.150/heat_status').subscribe(response => console.log(response));
-
-    return this.http.get<HeatStatusResponse>(this.heatStatusUrl).pipe(
+    return this.http.get<HeatStatusResponse>(this.heatControlStatusUrl).pipe(
       tap(_ => this.messageService.add('fetched status')),
       catchError(this.handleHttpError<HeatStatusResponse>('getHeatStatuses')));
-
-    //return this.http.get('http://192.168.1.150/heat_status');
-    /*.subscribe(res => {
-          q = res;
-          console.log("sus: " + q);
-        }
-        );
-    
-        console.log("end");
-    
-    
-        //console.log(q.heat_status);
-    
-        console.log("q: " + q['heat_status']);
-        //this.http.get(this.heatStatusUrl).subscribe(respons=>x=respons);
-    
-        //let r: String;
-        this.http.get<String>(this.heatStatusUrl).pipe(
-          tap(_ => this.messageService.add('fetched status')),
-          catchError(this.handleHttpError('getHeatStatuses', []))
-        ).subscribe(response => r = response);
-    
-        // console.log("x: " + x);
-        //  console.log("r: " + this.r);
-    
-        let heat_status = q;
-        if (heat_status == 1 || heat_status == 3) {
-          status1.enabled = true;
-        }
-        else if (heat_status == 0 || heat_status == 2) {
-          status1.enabled = false;
-        }
-        else {
-          //Error
-          status1.enabled = true;
-        }
-    
-        if (heat_status == 2 || heat_status == 3) {
-          status2.enabled = true;
-        }
-        else if (heat_status == 0 || heat_status == 1) {
-          status2.enabled = false;
-        } else {
-          //Error
-          status2.enabled = true;
-        }
-    
-    
-        //let q = JSON.stringify(y);
-        //console.log("q: " + q);
-        //let z = r[0];
-        //console.log("z: " + z.heat_status);
-        //let z = y["heat_status"];
-        //console.log(z);
-    
-    
-        status3.enabled = Math.random() < 0.5;
-    
-        return [status1, status2, status3]
-        */
   }
 
   getTasks(): Observable<Task[]> {
@@ -107,17 +40,9 @@ export class HeatControlService {
   }
 
   setPermanentOverride(state: boolean): Observable<string> {
-    this.messageService.add(`Permanente override aangeroepen`);
-
     this.messageService.add(`Permanente override ${state ? "aan" : "uit"} gezet`);
 
-    let x = {
-      type: "permanent",
-      state: state,
-    }
-    console.log("setPermanentOverride is calling " + "http://192.168.1.150/overrides?onzin=1&permanent=" + state)
-
-    return this.http.get<string>("http://192.168.1.150/overrides?onzin=1&permanent=" + state).pipe(
+    return this.http.get<string>(this.heatControlOverrideUrl + "permanent=" + state).pipe(
       tap((msg: string) => this.messageService.add(`Permanent override ingesteld, response: ${msg}`)),
       catchError(this.handleHttpError<string>('setPermanentOverride')));
   }
@@ -125,32 +50,21 @@ export class HeatControlService {
   setTemporaryOverride(state: boolean): Observable<string> {
     this.messageService.add(`Tijdelijke override ${state ? "aan" : "uit"} gezet`);
 
-    let x = {
-      type: "temporary",
-      state: state,
-    }
-    console.log("setTemporaryOverride is calling " + "http://192.168.1.150/overrides?onzin=1&temporary=" + state)
-
-    return this.http.get<string>("http://192.168.1.150/overrides?onzin=1&temporary=" + state).pipe(
+    return this.http.get<string>(this.heatControlOverrideUrl + "temporary=" + state).pipe(
       tap((msg: string) => this.messageService.add(`Override ingesteld, response: ${msg}`)),
       catchError(this.handleHttpError<string>('setTemporaryOverride')));
-
-    //return this.http.post<string>(this.heatControlOverridesUrl, x, httpOptions).pipe(
-    //      tap((msg: string) => this.messageService.add(`Override ingesteld, response: ${msg}`)),
-    //catchError(this.handleHttpError<string>('setTemporaryOverride')));
   }
-
-
+  
   addTask(task: Task): Observable<string> {
     this.messageService.add(`addTask called with time ${task.datetime} and action ${task.action}`);
 
-    let x = {
+    let newTask = {
       hour: task.datetime.getHours,
       min: task.datetime.getMinutes,
       action: task.action
     }
 
-    return this.http.post<string>(this.heatControlTasksUrl, x, httpOptions).pipe(
+    return this.http.post<string>(this.heatControlTasksUrl, newTask, httpOptions).pipe(
       tap((msg: string) => this.messageService.add(`Taak toegevoegd, response: ${msg}`)),
       catchError(this.handleHttpError<string>('addTask')));
   }
